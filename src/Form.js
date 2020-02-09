06f9578445a0b8fe417f418830b5db992dd4c138
Andrej <andrej.kasic7@gmail.com>
2020-02-09 12:18:40 +0100

    Form validation with Formik and Yup

--- src/Form.js
+++ src/Form.js
@@ -1,42 +1,63 @@
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import React, { useState } from "react";
-
-const initialState = {
-  title: "",
-  publishedYearFrom: ""
-};
+import { useFormik } from "formik";
+import React from "react";
+import * as Yup from "yup";
 
 const Form = ({ handleSubmit }) => {
-  const [state, setState] = useState({ title: "", publishedYearFrom: "" });
-
-  const handleChange = event => {
-    const { name, value } = event.target;
-
-    setState({
-      ...state,
-      [name]: value
-    });
-  };
-
-  const submitForm = () => {
-    handleSubmit(state);
-    setState(initialState);
-  };
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      publishedYearFrom: ""
+    },
+    validationSchema: Yup.object({
+      title: Yup.string()
+        .min(5, "Must be at least 5 characters")
+        .required("Required"),
+      publishedYearFrom: Yup.number()
+        .min(1900, "Must be 1900 or more")
+        .max(2100, "Must be 2100 or less")
+        .required("Required")
+    }),
+    onSubmit: values => {
+      handleSubmit(values);
+      formik.resetForm();
+    }
+  });
 
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <label htmlFor="title">Title</label>
-      <TextField required type="text" name="title" id="title" value={state.title} label="Title" variant="outlined" size="small" fullWidth onChange={handleChange} />
+      <TextField
+        type="text"
+        name="title"
+        id="title"
+        label="Title"
+        variant="outlined"
+        size="small"
+        fullWidth
+        value={formik.values.title}
+        onChange={formik.handleChange}
+      />
+      {formik.touched.title && formik.errors.title ? (
+        <div>{formik.errors.title}</div>
+      ) : null}
       <label htmlFor="publishedYearFrom">Year</label>
-      <TextField required type="number" name="publishedYearFrom" id="publishedYearFrom" value={state.publishedYearFrom} label="Year" variant="outlined" size="small" fullWidth onChange={handleChange} />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        value="Submit"
-        onClick={submitForm}
-      >
+      <TextField
+        type="number"
+        name="publishedYearFrom"
+        id="publishedYearFrom"
+        label="Year"
+        variant="outlined"
+        size="small"
+        fullWidth
+        value={formik.values.publishedYearFrom}
+        onChange={formik.handleChange}
+      />
+      {formik.touched.publishedYearFrom && formik.errors.publishedYearFrom ? (
+        <div>{formik.errors.publishedYearFrom}</div>
+      ) : null}
+      <Button type="submit" variant="contained" color="primary" value="Submit">
         Submit
       </Button>
     </form>
