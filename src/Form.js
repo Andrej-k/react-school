cf12450e2b2b57ec3751db07b435ad803f1f0778
Andrej <andrej.kasic7@gmail.com>
2020-02-09 17:44:07 +0100

    Update form sytle

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,23 @@
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { useFormik } from "formik";
 import React from "react";
 import * as Yup from "yup";
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginTop: "50px"
+  },
+  button: {
+    marginTop: "20px"
+  },
+  errorText: {
+    color: "red",
+    fontFamily: "Roboto",
+    fontSize: "12px"
+  }
+}));
 
 const Form = ({ handleSubmit }) => {
   const formik = useFormik({
@@ -25,39 +40,61 @@ const Form = ({ handleSubmit }) => {
     }
   });
 
+  const classes = useStyles();
+
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="title">Title</label>
+    <form onSubmit={formik.handleSubmit} className={classes.form}>
       <TextField
+        error={formik.touched.title && (formik.errors.title ? true : false)}
         type="text"
         name="title"
         id="title"
         label="Title"
+        placeholder="Enter title"
         variant="outlined"
         size="small"
         fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true
+        }}
         value={formik.values.title}
         onChange={formik.handleChange}
       />
       {formik.touched.title && formik.errors.title ? (
-        <div>{formik.errors.title}</div>
+        <div className={classes.errorText}>{formik.errors.title}</div>
       ) : null}
-      <label htmlFor="publishedYearFrom">Year</label>
       <TextField
+        error={
+          formik.touched.publishedYearFrom &&
+          (formik.errors.publishedYearFrom ? true : false)
+        }
         type="number"
         name="publishedYearFrom"
         id="publishedYearFrom"
         label="Year"
+        placeholder="Enter year"
         variant="outlined"
         size="small"
         fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true
+        }}
         value={formik.values.publishedYearFrom}
         onChange={formik.handleChange}
       />
       {formik.touched.publishedYearFrom && formik.errors.publishedYearFrom ? (
-        <div>{formik.errors.publishedYearFrom}</div>
+        <div className={classes.errorText}>
+          {formik.errors.publishedYearFrom}
+        </div>
       ) : null}
-      <Button type="submit" variant="contained" color="primary" value="Submit">
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
         Submit
       </Button>
     </form>
